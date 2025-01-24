e4ed3c98239fbdb8c30321ddccd57887177fea08
Beomsuk Seo <beomsukbrendanseo@gmail.com>
2025-01-24 07:13:16 -0800

    global.js update

--- global.js
+++ global.js
@@ -3,3 +3,23 @@ console.log('IT’S ALIVE!');
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
+
+// step 2
+// navLinks = $$("nav a");
+// currentLink?.classList.add('current');
+
+let pages = [
+  {url: '', title: 'Home'},
+  {url: 'projects/', title: 'Projects'},
+  {url: 'resume/', title: 'Resume'},
+  {url: 'contact/', title: 'Contact'}
+];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+  let url = p.url;
+  let title = p.title;
+  nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+}
